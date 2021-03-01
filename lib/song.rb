151ab5cc25f9d1503452dd905a63e60ed7386be4
github username <dreedsanders@gmail.com>
2021-03-01 14:42:30 -0600

    commit tree e2655e26fd91776c2a01e5808d2b62116b69b212

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,14 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+
+  def initialize (name = nil, artist_name = nil)
+    @name = name 
+    @artist_name = artist_name 
+  end
+
 
   def self.all
     @@all
@@ -10,4 +18,39 @@ class Song
     self.class.all << self
   end
 
+  def self.create 
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    self.new(name)
+  end
+
+  def self.create_by_name(name)
+    song = self.new(name)
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find {|songs| songs.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if Song.find_by_name(name) == name
+     Song.find_by_name(name)
+    else
+    Song.find_by_name(name) != name
+      song = Song.create_by_name("name")
+      self
+    end
+
 end
+
+
+end
+
+# song=Song.new("mememe")
+# binding.pry
